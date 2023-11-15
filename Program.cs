8e018b0950bbb3c7b7290c26fc9c941f56050d2f
x00191169 <x00191169@T20-WP2-216-19>
2023-11-15 13:43:35 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+
+
+namespace UnitTestLab
+{
+
+    public class BankAccount
+    {
+        private string sortCode;
+        private string accountNumber;
+        private double overdraftLimit;
+        private double balance;
+        private List<double> history;
+
+
+        public BankAccount(string sortCode, string accountNumber, double overdraftLimit, double balance)
+        {
+            this.sortCode = sortCode;
+            this.accountNumber = accountNumber;
+            this.overdraftLimit = overdraftLimit;
+            this.balance = 0;
+            this.history = new List<double>();
+        }
+
+        public BankAccount(string sortCode, string accountNumber) : this(sortCode, accountNumber, 0, 0){ }
+
+        public string SortCode { get { return sortCode; } }
+        public string AccountNumber { get { return accountNumber; } }
+        public double OverdraftLimit { get { return overdraftLimit; } }
+        public double Balance {get { return balance; } }
+        public List<double> History { get{ return history; } }
+
+
+
+        public void Deposit(double amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentException("Amount cannot be 0");
+            }
+            else
+            {
+                balance += amount;
+                history.Add(amount);
+            }
+
+        }
+
+        public void Withdraw(int amount)
+        {
+            if(amount <= balance + overdraftLimit)
+            {
+                balance -= amount;
+                history.Add(-amount);
+            }
+            else
+            {
+                throw new ArgumentException("Insufficient balance");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"SortCode: {sortCode}\nAccount Number: {accountNumber}\nOverdraft Limit: {overdraftLimit}\nBalance: {balance}\nHistory: {history}";
+        }
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ TestClass.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestLab
+{
+    [TestClass]
+    public class BankUnitTest
+    {
+        [TestMethod]
+        public void TestDeposit()
+        {
+            BankAccount a1 = new BankAccount("AIB2kI3", "x9223T");
+            a1.Deposit(100);
+            a1.Deposit(100);
+            Assert.AreEqual(a1.Balance, 200);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestDepositException() 
+        {
+            BankAccount a1 = new BankAccount("AIB2kI3", "x9223T", 100, 200);
+            a1.Deposit(-100);
+            
+        }
+
+
+        [TestMethod]
+        public void TestWidrawal()
+        {
+            BankAccount a1 = new BankAccount("AIB2kI3", "x9223T");
+            a1.Withdraw(100);
+            a1.Withdraw(20);
+            Assert.AreEqual(a1.Balance, 80);
+        }
+
+        [TestMethod]
+        [ExpectedException (typeof(ArgumentException))]
+        public void TestWidrawalException()
+        {
+            BankAccount a1 = new BankAccount("AIB2kI3", "x9223T", 200, 500);
+            a1.Withdraw(6000);
+        }
+
+        [TestMethod]
+        public void TestHistory()
+        {
+            BankAccount a1 = new BankAccount("AIB2kI3", "x9223T");
+            a1.Deposit(2000);
+            a1.Withdraw(1000);
+            a1.Withdraw(20);
+            a1.Deposit(75);
+            a1.Withdraw(10);
+            Assert.AreEqual(a1.Balance, 1045);
+            CollectionAssert.AreEqual(a1.History, new List<double>() { 2000, -1000, 20, 75, 10 });
+        }
+
+        [TestMethod]
+        public void TestAccountHistory()
+        {
+            BankAccount a1 = new BankAccount("AIB2kI3", "x9223T", 0, 0);
+            Assert.AreEqual(a1.Balance, 0);
+            CollectionAssert.AreEqual(a1.History, new List<double>() { 0 });
+        }
+    }
+
+}
